42f0f39118a543315de8229aeb650c9e6242b60b
Felipe Garcia <felipegarcia1402@gmail.com>
2022-10-23 15:29:36 -0300

    code refactor

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,22 +2,22 @@
 
 namespace zbbm::detail
 {
-    void write(std::ostream& handle, const book& obook)
+    void write(std::ostream& handle, const book& target_book)
     {
-        handle.write(obook.isbn.c_str(), obook.isbn.size());
-        handle.write(obook.name.c_str(), obook.name.size());
-        handle.write(obook.author.c_str(), obook.author.size());
+        handle.write(target_book.isbn.c_str(), target_book.isbn.size());
+        handle.write(target_book.name.c_str(), target_book.name.size());
+        handle.write(target_book.author.c_str(), target_book.author.size());
         
-        auto n_authors {obook.co_authors.size()};
+        auto n_authors{ target_book.co_authors.size() };
         handle.write(reinterpret_cast<char *>(&n_authors), sizeof(n_authors));
 
-        for(auto idx{ 0U }; idx < n_authors; ++idx)
-            handle.write(obook.co_authors[idx].c_str(), 
-                         obook.co_authors[idx].size());
-            
+        for (const auto& coauthor : target_book.co_authors)
+            handle.write(coauthor.c_str(), coauthor.size());
         
-        handle.write(obook.publisher.c_str(), obook.publisher.size());
-        handle.write(obook.launch_date.c_str(), obook.launch_date.size());
-        handle.write(obook.language.c_str(), obook.language.size());
+        handle.write(target_book.publisher.c_str(), target_book.publisher.size());
+        handle.write(target_book.launch_date.c_str(), target_book.launch_date.size());
+        handle.write(target_book.language.c_str(), target_book.language.size());
+
+        handle.flush();
     }
 }
