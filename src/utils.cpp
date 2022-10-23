1d42879d4b402b1e095cd62a75aeaff74c3de2d6
Bryan K <78167383+BryanProg@users.noreply.github.com>
2022-10-23 15:14:15 -0300

    update write

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,18 +5,20 @@ namespace zbbm::detail
 {
     void write(std::ostream& handle, const book& obook)
     {
-        handle << obook.isbn;
-        handle << obook.name;
-        handle << obook.author;
+        handle.write(obook.isbn.c_str(), obook.isbn.size());
+        handle.write(obook.name.c_str(), obook.name.size());
+        handle.write(obook.author.c_str(), obook.author.size());
         
         auto n_authors {obook.co_authors.size()};
-        handle << n_authors;
+        handle.write(reinterpret_cast<char *>(&n_authors), sizeof(n_authors));
 
         for(auto idx{ 0U }; idx < n_authors; ++idx)
-            handle << obook.co_authors[idx];
+            handle.write(obook.co_authors[idx].c_str(), 
+                         obook.co_authors[idx].size());
+            
         
-        handle << obook.publisher;
-        handle << obook.launch_date;
-        handle << obook.language;
+        handle.write(obook.publisher.c_str(), obook.publisher.size());
+        handle.write(obook.launch_date.c_str(), obook.launch_date.size());
+        handle.write(obook.language.c_str(), obook.language.size());
     }
 }
