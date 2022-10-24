dac9525125ad14e0968479ac8161edac26d202e4
João Gerliandro <joaogerliandro@gmail.com>
2022-10-24 16:23:05 -0300

    naive solution for option handling

--- include/user_interface.hpp
+++ include/user_interface.hpp
@@ -5,7 +5,51 @@
 
 namespace zbbm::interface
 {
-    static void display_user_interface()
+    enum InterfaceOption 
+    {
+        EXIT,
+        ADD,
+        REMOVE,
+        FIND,
+        EDIT,
+        LIST,
+        CLEAR,
+        SAVE
+    };
+
+    void option_handler(bool *running)
+    {
+        int32_t choice;
+        std::cin >> choice;
+
+        switch (choice)
+        {
+            case EXIT:
+                *running = false;
+                std::cout << "\n[INFO]: You exited the system, come back soon !" << "\n";
+                break;
+            case ADD:
+                break;
+            case REMOVE:
+                break;
+            case FIND:
+                break;
+            case EDIT:
+                break;
+            case LIST:
+                break;
+            case CLEAR:
+                break;
+            case SAVE:
+                break;
+        
+            default:
+                std::cout << "\n[ERRO]: Invalid option, try again !" << "\n\n";
+                break;
+        }
+    }
+
+    void show_menu_interface()
     {
         std::cout << "==================== Ze Bigode's Book Manager ====================" << "\n";
 
@@ -20,6 +64,15 @@ namespace zbbm::interface
                   << "0 - Exit Ze Bigodes Book Manager"           << "\n"
                   << "---------- Management Options ----------"   << "\n";
 
-        std::cout << "\nEnter your choice: " << "\n";
+        std::cout << "\nEnter your choice: ";
+    }
+
+    static void display_user_interface()
+    {
+        for( bool running = true; running; )
+        {
+            zbbm::interface::show_menu_interface();
+            zbbm::interface::option_handler(&running);
+        }
     }
 }
