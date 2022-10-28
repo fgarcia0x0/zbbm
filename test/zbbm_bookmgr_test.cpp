414a20b60290bcb2b8ef39e0aa250d65af2e425c
Bryan K <78167383+BryanProg@users.noreply.github.com>
2022-10-28 20:38:55 -0300

    FindTest and ListTest

--- test/zbbm_bookmgr_test.cpp
+++ test/zbbm_bookmgr_test.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <book_manager.h>
 #include <gtest/gtest.h>
+#include <vector>
 
 static void edit_book(zbbm::book& book)
 {
@@ -15,14 +16,14 @@ TEST(BookManagerTest, AddTest)
 {
     zbbm::book_manager book_manager{ "BookDatabaseTest.zbbm" };
     zbbm::book book{};
-    
-    book.author = "AuthorTest";
-    book.co_authors = { "CoAuthor1", "CoAuthor2" };
-    book.isbn = "994957872341";
-    book.language = "pt-br";
-    book.launch_date = "20/11/2022";
-    book.name = "BookName";
-    book.publisher = "PublisherName";
+
+    book.author         = "AuthorTest";
+    book.co_authors     = { "CoAuthor1", "CoAuthor2" };
+    book.isbn           = "994957872341";
+    book.language       = "pt-br";
+    book.launch_date    = "20/11/2022";
+    book.name           = "BookName";
+    book.publisher      = "PublisherName";
     
     book_manager.add(book);
 
@@ -79,10 +80,34 @@ TEST(BookManagerTest, RemoveTest)
 
 TEST(BookManagerTest, ListTest)
 {
+    zbbm::book_manager book_manager{ "BookDatabaseTestList.zbbm" };
+    constexpr const uint8_t n_books {10u};
+    std::vector<zbbm::book> books(n_books);
 
+    for(uint8_t idx{}; idx < n_books; ++idx)
+    {
+        books[idx].author         = std::string("AuthorTest");
+        books[idx].co_authors     = { "CoAuthor1", "CoAuthor2" };
+        books[idx].isbn           = std::string("451400") + std::to_string(idx);
+        books[idx].language       = "ptbr";
+        books[idx].launch_date    = "20/11/2022";
+        books[idx].name           = "BookName";
+        books[idx].publisher      = "PublisherName";
+    }
+    
+    for(const auto& book : books)
+        book_manager.add(book);
+
+
+    for (uint8_t idx {}; idx < books.size(); ++idx)
+    {
+        auto result = book_manager.find(books[idx].isbn);
+        EXPECT_TRUE(result != std::nullopt);
+        EXPECT_EQ(result.value(), books[idx]);
+    }
 };
 
-TEST(BookManagerTest, FindTest)
+TEST(BookManagerTest, UpdateTest)
 {
     //EXPECT_EQ(1000, cubic(10));
 };
@@ -140,10 +165,36 @@ TEST(BookManagerTest, SaveInPathTest)
 
 };
 
-TEST(BookManagerTest, UpdateTest)
+TEST(BookManagerTest, FindTest)
 {
-    zbbm::book_manager book_manager{ "BookDatabaseTest.zbbm" };
+    zbbm::book_manager book_manager{ "BookDatabaseTestfind.zbbm" };
+    zbbm::book book_test_1{};
+    zbbm::book book_test_2{};
+    
+    book_test_1.author      = "AuthorTest1";
+    book_test_1.co_authors  = { "CoAuthor1", "CoAuthor2" };
+    book_test_1.isbn        = "994957872341";
+    book_test_1.language    = "ptbr";
+    book_test_1.launch_date = "20/11/2022";
+    book_test_1.name        = "BookTest1Name";
+    book_test_1.publisher   = "PublisherName";
 
-    // ???????
+    book_test_2.author      = "AuthorTest2";
+    book_test_2.co_authors  = { "CoAuthor1", "CoAuthor2" };
+    book_test_2.isbn        = "99486775425";
+    book_test_2.language    = "En";
+    book_test_2.launch_date = "23/11/2002";
+    book_test_2.name        = "BookTest2Name";
+    book_test_2.publisher   = "PublisherName2";
+    
+    book_manager.add(book_test_1);
+    book_manager.add(book_test_2);
 
+    auto result = book_manager.find("994957872341");
+    EXPECT_TRUE(result != std::nullopt);
+    EXPECT_FALSE(result.value().isbn == book_test_2.isbn);
+
+    result = book_manager.find(book_test_2.isbn);
+    EXPECT_TRUE(result != std::nullopt);
+    EXPECT_EQ(result.value(), book_test_2);
 };
